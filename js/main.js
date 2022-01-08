d31e31c3581a345ae88d052faaa6af79d700536e
bubbzDotDev <christopher.blount@outlook.com>
2022-01-07 20:42:14 -0600

    Built out dynamic table of contents

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Build table of contents
+const displayLinks = (items, list) => {
+  items.forEach(item => {
+    const li = document.createElement('li');
+    li.innerHTML = `
+      <a href="${item.url}">${item.label}</a>
+    `;
+    list.appendChild(li);
+  });
+}
+
+// Fetch the data
+fetch('data/data.json')
+.then(response => response.json())
+.then(data => {
+  const links = data.links;
+  const linksList = document.querySelector('#links-list');
+  displayLinks(links, linksList);
+});
