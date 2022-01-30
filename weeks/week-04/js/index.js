cda1eff026d5d270a1f359470bc4c30a55d300a3
bubbzDotDev <christopher.blount@outlook.com>
2022-01-29 23:00:22 -0600

    Week 4 complete

---
+++ weeks/week-04/js/index.js
@@ -0,0 +1,33 @@
+const add = (strToAdd) => {
+  const arrFromStr = strToAdd.split('+');
+  let result = 0;
+  arrFromStr.forEach(num => {
+    result += +num;
+  });
+  return result;
+}
+
+const submitButton = document.querySelector('#submit-button');
+
+submitButton.addEventListener('click', () => {
+  const numberInput = document.querySelector('#number-input').value;
+  const displayResult = document.querySelector('#display-result');
+
+  if (numberInput === '') {
+    displayResult.innerHTML = '<span class="error">Enter a number and try again!</span>';
+  } else {
+    const result = add(numberInput);
+    displayResult.innerHTML = `The result is <strong>${result}</strong>.`
+  }
+});
+
+const displayButton = document.querySelector('#display-button');
+
+displayButton.addEventListener('click', () => {
+  const displayFunction = document.querySelector('#display-function');
+  displayFunction.classList.toggle('hide-me');
+
+  const showHideSpan = document.querySelector('#show-hide-span');
+  displayFunction.classList.contains('hide-me') ? showHideSpan.textContent = 'Display' : showHideSpan.textContent = 'Hide';
+});
+
