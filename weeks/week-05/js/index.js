cddf25699bb310a878a3ae46d438b9a6b862fbfa
bubbzDotDev <christopher.blount@outlook.com>
2022-02-05 00:16:20 -0600

    Setup weeks 5 & 6. Finished week 5.

---
+++ weeks/week-05/js/index.js
@@ -0,0 +1,32 @@
+const addTo = (num) => {
+  let result = 0;
+  for (let i = 1; i <= num; i++) {
+    result += i;
+  }
+  return result;
+}
+
+const submitButton = document.querySelector('#submit-button');
+
+submitButton.addEventListener('click', () => {
+  const numberInput = document.querySelector('#number-input').value;
+  const displayResult = document.querySelector('#display-result');
+
+  if (numberInput === '') {
+    displayResult.innerHTML = '<span class="error">Enter a number and try again!</span>';
+  } else {
+    const result = addTo(numberInput);
+    displayResult.innerHTML = `The result is <strong>${result}</strong>.`
+  }
+});
+
+const displayButton = document.querySelector('#display-button');
+
+displayButton.addEventListener('click', () => {
+  const displayFunction = document.querySelector('#display-function');
+  displayFunction.classList.toggle('hide-me');
+
+  const showHideSpan = document.querySelector('#show-hide-span');
+  displayFunction.classList.contains('hide-me') ? showHideSpan.textContent = 'Display' : showHideSpan.textContent = 'Hide';
+});
+
